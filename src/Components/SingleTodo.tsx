d8f6b6aa788d7747b39612fe089bc6a2e4760888
Lipatov <lipatovnn@yahoo.com>
2022-05-31 21:13:33 +0300

    edit, delete, add functionality added

--- src/Components/SingleTodo.tsx
+++ src/Components/SingleTodo.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState, useReducer } from 'react';
 import { Todo } from '../Model';
 import {AiFillDelete, AiFillEdit} from 'react-icons/ai';
 import {MdDone} from 'react-icons/md';
@@ -8,41 +8,61 @@ type Props = {
     todo: Todo,
     todos: Todo[],
     setTodos: React.Dispatch<React.SetStateAction<Todo[]>>
-
 }
 
 const SingleTodo:React.FC<Props> = ({todo, todos, setTodos}) => {
     const [edit, setEdit] = useState<boolean>(false);
     const [editTodo, setEditTodo] = useState<string>(todo.todo);
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    useEffect(() => {
+        inputRef.current?.focus();
+    }, [edit])
 
     const handleDone = (id: number) =>{
         setTodos(
             todos.map((todo)=>
             todo.id === id ? {...todo, isDone: !todo.isDone} : todo)
         )
-    }
+    };
 
     const handleDelete = (id: number) =>{
         setTodos(
             todos.filter((todo) => todo.id !== id)
         )
-    }
+    };
 
-
+    const handleEdit = (e:React.FormEvent, id: number) => {
+        e.preventDefault();
+        setTodos(
+            todos.map(todo =>
+                todo.id === id ? {...todo, todo: editTodo} : todo
+            )
+        )
+        setEdit(false);
+    };
 
     return(
-        <form className='todos__single'>
+        <form className='todos__single' onSubmit={(e) => handleEdit(e, todo.id)}>
             {
-                todo.isDone? (
-                    <s className='todos__single--text'>
-                        {todo.todo}
-                    </s>
-                )
-                :
+                edit ? (
+                    <input ref={inputRef}
+                        value={editTodo}
+                        onChange={(e) => setEditTodo(e.target.value)}
+                        className="todos__single--text"/>
+                ):
                 (
-                    <span className='todos__single--text'>
-                        {todo.todo}
-                    </span>
+                    todo.isDone? (
+                        <s className='todos__single--text'>
+                            {todo.todo}
+                        </s>
+                    )
+                    :
+                    (
+                        <span className='todos__single--text'>
+                            {todo.todo}
+                        </span>
+                    )
                 )
             }
 
